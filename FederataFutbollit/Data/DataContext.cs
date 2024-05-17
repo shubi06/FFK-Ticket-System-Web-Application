e71991aee8f3d159d5cd262bd699d9ac4b76c675
Shuajb Bunjaku <bunjakushuajb@gmail.com>
2024-05-17 19:52:55 +0200

    Git Add

--- FederataFutbollit/Data/DataContext.cs
+++ FederataFutbollit/Data/DataContext.cs
@@ -12,17 +12,15 @@ namespace FederataFutbollit.Data
         public DbSet<Superliga> Superligat { get; set; }
         public DbSet<Selektori>Selektort{get;set;}
         public DbSet<Stafi> Stafi { get; set; }
-
-<<<<<<< HEAD
          
-=======
+
         public DbSet<Shteti> Shteti { get; set; }
 
         public DbSet<Kombetarja> Kombetarja { get; set; }
 
         public DbSet<Lojtaret> Lojtaret { get; set; }
 
->>>>>>> d3f25df74f18502bf39415111902c9e358b4b582
+
     }
 
 
