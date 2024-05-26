284c3283ce29a613ef95107456005855458b1327
Gentrit2023 <gentrit.gashi@mail.com>
2024-05-26 23:17:16 +0200

    Update ne Realcionin e userit me bilet

---
+++ FederataFutbollit/Migrations/20240526210010_ShtoRelacionetUserBileta.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FederataFutbollit.Migrations
+{
+    /// <inheritdoc />
+    public partial class ShtoRelacionetUserBileta : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ApplicationUserID",
+                table: "Biletat",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Biletat_ApplicationUserID",
+                table: "Biletat",
+                column: "ApplicationUserID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Biletat_AspNetUsers_ApplicationUserID",
+                table: "Biletat",
+                column: "ApplicationUserID",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Biletat_AspNetUsers_ApplicationUserID",
+                table: "Biletat");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Biletat_ApplicationUserID",
+                table: "Biletat");
+
+            migrationBuilder.DropColumn(
+                name: "ApplicationUserID",
+                table: "Biletat");
+        }
+    }
+}
