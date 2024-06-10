a7d1d8b7a0f44fb8716f30f7cb74b472d9fa7c37
anesamustafa1 <anesa1.mustafa@gmail.com>
2024-06-10 22:52:00 +0200

    Full update for Referi

---
+++ FederataFutbollit/DTOs/ReferiCreateDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FederataFutbollit.DTOs
+{
+    public class ReferiCreateDto
+    {
+        public string Emri { get; set; }
+
+        public string Mbiemri { get; set; }
+
+
+        public string Kombesia { get; set; }
+
+
+        public int Mosha { get; set; }
+    }
+}
